eda16d4703a5cd59c6af21c365022595fd36fd04
MateuszDyla <mdyla6@gmail.com>
2021-11-10 23:01:10 +0100

    Add FinalClassInEntitiesOrRepositoriesFixer

---
+++ src/Fixer/FinalClassInEntitiesOrRepositoriesFixer.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Fixer;
+
+use PhpCsFixer\Fixer\FixerInterface;
+use PhpCsFixer\FixerDefinition\CodeSample;
+use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
+use PhpCsFixer\Tokenizer\Tokens;
+use SplFileInfo;
+use const T_FINAL;
+
+final class FinalClassInEntitiesOrRepositoriesFixer implements FixerInterface
+{
+    public function isCandidate(Tokens $tokens): bool
+    {
+        return $tokens->isTokenKindFound(T_FINAL);
+    }
+
+    public function isRisky(): bool
+    {
+        return true;
+    }
+
+    public function fix(SplFileInfo $file, Tokens $tokens): void
+    {
+        for ($index = $tokens->count() - 1; 0 <= $index; --$index) {
+            if (!$tokens[$index]->isGivenKind(T_FINAL)) {
+                continue;
+            }
+            $tokens->clearAt($index);
+
+            if ($tokens[$index - 1]->isGivenKind(\T_WHITESPACE)) {
+                $tokens->clearAt($index - 1);
+            }
+        }
+    }
+
+    public function getDefinition(): FixerDefinitionInterface
+    {
+        return new FixerDefinition(
+            'Repositories and Entities should not be defined as final.',
+            [
+                new CodeSample(
+                    '<?php
+
+                           declare(strict_types=1);
+
+                           namespace App\Entity;
+
+                           final class Product
+                           {
+
+                           }
+                           '
+                ),
+            ]
+        );
+    }
+
+    public function getName(): string
+    {
+        return self::class;
+    }
+
+    public function getPriority(): int
+    {
+        return 0;
+    }
+
+    public function supports(SplFileInfo $file): bool
+    {
+        return true;
+    }
+}
