609aa8538347a03cbd6009605984f23f61494cde
MateuszDyla <mdyla6@gmail.com>
2021-11-19 10:13:15 +0100

    Fix PHPStan errors

--- tests/Application/config/sylius/1.8/bundles.php
+++ tests/Application/config/sylius/1.8/bundles.php
@@ -2,9 +2,20 @@
 
 declare(strict_types=1);
 
-return [
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
-    WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
-    FOS\OAuthServerBundle\FOSOAuthServerBundle::class => ['all' => true],
-    Sylius\Bundle\AdminApiBundle\SyliusAdminApiBundle::class => ['all' => true],
-];
+$bundles = [];
+
+if (class_exists('Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle')) {
+    $bundles[Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class] = ['all' => true];
+}
+if (class_exists('WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle')) {
+    $bundles[WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class] = ['all' => true];
+}
+if (class_exists('FOS\OAuthServerBundle\FOSOAuthServerBundle')) {
+    $bundles[FOS\OAuthServerBundle\FOSOAuthServerBundle::class] = ['all' => true];
+}
+if (class_exists('Sylius\Bundle\AdminApiBundle\SyliusAdminApiBundle')) {
+    $bundles[Sylius\Bundle\AdminApiBundle\SyliusAdminApiBundle::class] = ['all' => true];
+}
+dump($bundles);
+
+return $bundles;
