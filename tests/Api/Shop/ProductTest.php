34a00c50c6f6adebaa1da01d21e7fe2093785b9a
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2021-12-03 16:25:38 +0100

    Add API tests for getting product bundle from product resource

---
+++ tests/Api/Shop/ProductTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\BitBag\SyliusProductBundlePlugin\Api\Shop;
+
+use ApiTestCase\JsonApiTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+final class ProductTest extends JsonApiTestCase
+{
+    public const CONTENT_TYPE_HEADER = ['CONTENT_TYPE' => 'application/ld+json', 'HTTP_ACCEPT' => 'application/ld+json'];
+
+    protected function setUp(): void
+    {
+        $this->loadFixturesFromFiles(['general/channels.yml', 'shop/product_bundles.yml']);
+    }
+
+    /** @test */
+    public function it_gets_bundled_product(): void
+    {
+        $this->client->request(
+            'GET',
+            '/api/v2/shop/products/WHISKEY_DOUBLE_PACK',
+            [],
+            [],
+            self::CONTENT_TYPE_HEADER
+        );
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'shop/get_bundled_product_response', Response::HTTP_OK);
+    }
+}
