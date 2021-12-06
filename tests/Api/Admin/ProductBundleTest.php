9b70a192d43101c69d5f9162105bd9ddc2162da5
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2021-12-06 13:08:43 +0100

    Add GET /admin/product-bundles/{id} endpoint

--- tests/Api/Admin/ProductBundleTest.php
+++ tests/Api/Admin/ProductBundleTest.php
@@ -10,10 +10,11 @@ use Symfony\Component\HttpFoundation\Response;
 final class ProductBundleTest extends JsonApiTestCase
 {
     public const CONTENT_TYPE_HEADER = ['CONTENT_TYPE' => 'application/ld+json', 'HTTP_ACCEPT' => 'application/ld+json'];
+    private $fixtures = [];
 
     protected function setUp(): void
     {
-        $this->loadFixturesFromFiles(['general/channels.yml', 'shop/product_bundles.yml']);
+        $this->fixtures = $this->loadFixturesFromFiles(['general/channels.yml', 'shop/product_bundles.yml']);
     }
 
     /** @test */
@@ -29,5 +30,22 @@ final class ProductBundleTest extends JsonApiTestCase
         $response = $this->client->getResponse();
 
         $this->assertResponse($response, 'admin/get_product_bundle_collection_response', Response::HTTP_OK);
+    }
+
+    /** @test */
+    public function it_gets_product_bundle(): void
+    {
+        $productBundleId = $this->fixtures['productBundle1']->getId();
+
+        $this->client->request(
+            'GET',
+            '/api/v2/admin/product-bundles/' . $productBundleId,
+            [],
+            [],
+            self::CONTENT_TYPE_HEADER
+        );
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'admin/get_product_bundle_response', Response::HTTP_OK);
     }
 }
