c10f72a64b7d8a677262c154fb921670b66aa413
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2021-12-15 15:53:36 +0100

    Add validation translations

--- src/Validator/ValidAddProductBundleToCartCommand.php
+++ src/Validator/ValidAddProductBundleToCartCommand.php
@@ -14,13 +14,13 @@ use Symfony\Component\Validator\Constraint;
 
 final class ValidAddProductBundleToCartCommand extends Constraint
 {
-    const NO_ORDER_ID_OR_TOKEN_MESSAGE = "Neither order's ID nor order's token provided";
-    const PRODUCT_BUNDLE_DOESNT_EXIST_MESSAGE = "Product bundle with id {{ id }} does not exist.";
-    const PRODUCT_DISABLED_MESSAGE = "Product with {{ code }} code is not enabled.";
-    const PRODUCT_VARIANT_DISABLED_MESSAGE = "Product Variant with {{ code }} code is not enabled.";
-    const CART_DOESNT_EXIST_MESSAGE = "Cart with provided ID or token does not exist.";
-    const PRODUCT_VARIANT_INSUFFICIENT_STOCK_MESSAGE = "Product variant with {{ code }} code does not have sufficient stock.";
-    const PRODUCT_DOESNT_EXIST_MESSAGE = "Product {{ name }} does not exist.";
+    const NO_ORDER_ID_OR_TOKEN_MESSAGE = "bitbag_sylius_product_bundle.add_to_cart.no_order_id_or_token";
+    const PRODUCT_BUNDLE_DOESNT_EXIST_MESSAGE = "bitbag_sylius_product_bundle.add_to_cart.product_bundle_doesnt_exist";
+    const PRODUCT_DISABLED_MESSAGE = "bitbag_sylius_product_bundle.add_to_cart.product_disabled";
+    const PRODUCT_VARIANT_DISABLED_MESSAGE = "bitbag_sylius_product_bundle.add_to_cart.product_variant_disabled";
+    const CART_DOESNT_EXIST_MESSAGE = "bitbag_sylius_product_bundle.add_to_cart.cart_doesnt_exist";
+    const PRODUCT_VARIANT_INSUFFICIENT_STOCK_MESSAGE = "bitbag_sylius_product_bundle.add_to_cart.product_variant_insufficient_stock";
+    const PRODUCT_DOESNT_EXIST_MESSAGE = "bitbag_sylius_product_bundle.add_to_cart.product_doesnt_exist";
 
     public function validatedBy(): string
     {
