48aa1a22c1d72cbd8460c46a3c2a40670ec577b5
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2021-12-29 12:58:15 +0100

    Add SequentiallyValidator to provide support for both Sf 4.4 and 5.x

---
+++ src/Validator/SequentiallyValidator.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+ */
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusProductBundlePlugin\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+final class SequentiallyValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint): void
+    {
+        if (!$constraint instanceof Sequentially) {
+            throw new UnexpectedTypeException($constraint, Sequentially::class);
+        }
+
+        $context = $this->context;
+
+        $validator = $context->getValidator()->inContext($context);
+
+        $originalCount = $validator->getViolations()->count();
+
+        foreach ($constraint->constraints as $c) {
+            if ($originalCount !== $validator->validate($value, $c)->getViolations()->count()) {
+                break;
+            }
+        }
+    }
+}
