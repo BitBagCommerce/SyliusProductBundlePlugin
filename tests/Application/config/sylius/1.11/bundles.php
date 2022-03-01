163cff23248ff402232c248291babede245a3131
marekrzytki <nooneusing@outlook.com>
2022-03-01 12:58:29 +0100

    Add support for Sylius 1.11

---
+++ tests/Application/config/sylius/1.11/bundles.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+$bundles = [];
+
+if (class_exists('BabDev\PagerfantaBundle\BabDevPagerfantaBundle')) {
+    $bundles[BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class] = ['all' => true];
+}
+if (class_exists('SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle')) {
+    $bundles[SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class] = ['all' => true];
+}
+if (class_exists('Sylius\Calendar\SyliusCalendarBundle')) {
+    $bundles[Sylius\Calendar\SyliusCalendarBundle::class] = ['all' => true];
+}
+
+return $bundles;
