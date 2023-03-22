6bfe678c7795b066e179a93dbaedd0e4dfaefbe5
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-03-22 07:28:35 +0100

    Added webpack encore to composer

--- tests/Application/config/sylius/1.11/bundles.php
+++ tests/Application/config/sylius/1.11/bundles.php
@@ -3,7 +3,8 @@
 declare(strict_types=1);
 
 $bundles = [
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true]
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true]
 ];
 
 if (class_exists('BabDev\PagerfantaBundle\BabDevPagerfantaBundle')) {
