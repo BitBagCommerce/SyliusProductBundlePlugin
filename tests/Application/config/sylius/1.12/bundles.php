6ca299304d826a292ff5e29ce7bb774cfb290fd3
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-03-22 11:56:56 +0100

    Fixed API

--- tests/Application/config/sylius/1.12/bundles.php
+++ tests/Application/config/sylius/1.12/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
 ];
