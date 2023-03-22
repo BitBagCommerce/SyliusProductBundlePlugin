a0f96dcd5e5f72aad9a0b32a67938c021aa6e0ce
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-03-22 08:49:16 +0100

    ECS

--- tests/Application/config/sylius/1.12/bundles.php
+++ tests/Application/config/sylius/1.12/bundles.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 return [
-    League\FlysystemBundle\FlysystemBundle::class => ['all'=>true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all'=> true],
+    League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
-    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true]
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];
