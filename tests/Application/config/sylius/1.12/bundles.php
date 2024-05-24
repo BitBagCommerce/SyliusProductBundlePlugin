5e85e5a2a4a641932dd0b27d74c8fcb489d5fd42
PiotrSzymanski2000 <piotreksz-18@wp.pl>
2024-05-24 09:19:25 +0200

    OP-270 fix pipeline

--- tests/Application/config/sylius/1.12/bundles.php
+++ tests/Application/config/sylius/1.12/bundles.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 return [
-    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
     Sylius\Abstraction\StateMachine\SyliusStateMachineAbstractionBundle::class => ['all' => true],
 ];
