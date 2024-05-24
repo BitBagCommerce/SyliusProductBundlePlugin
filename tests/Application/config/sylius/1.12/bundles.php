8418fae34d2d6f0260f73d8e5922aa510c54ed12
PiotrSzymanski2000 <piotreksz-18@wp.pl>
2024-05-24 08:01:47 +0200

    OP-270 Sylius 1.13 support

--- tests/Application/config/sylius/1.12/bundles.php
+++ tests/Application/config/sylius/1.12/bundles.php
@@ -3,5 +3,7 @@
 declare(strict_types=1);
 
 return [
-    League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
+    Sylius\Abstraction\StateMachine\SyliusStateMachineAbstractionBundle::class => ['all' => true],
 ];
