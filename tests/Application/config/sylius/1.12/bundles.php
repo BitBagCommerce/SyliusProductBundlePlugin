b5db16d6b515c42ff5dbb93e25ef9d2f6a03325b
PiotrSzymanski2000 <piotreksz-18@wp.pl>
2024-05-24 09:30:15 +0200

    OP-270 fix pipeline

--- tests/Application/config/sylius/1.12/bundles.php
+++ tests/Application/config/sylius/1.12/bundles.php
@@ -3,5 +3,5 @@
 declare(strict_types=1);
 
 return [
-    Sylius\Abstraction\StateMachine\SyliusStateMachineAbstractionBundle::class => ['all' => true],
+    League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
 ];
