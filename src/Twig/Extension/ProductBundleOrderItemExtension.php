bb778ca07f626b90019c6e6c306e26dfd00bb4cc
Hubert Filar <hubert.filar@gmail.com>
2024-08-26 12:31:03 +0200

    OP-289: Add accordion with bundled products on order view

--- src/Twig/Extension/ProductBundleOrderItemExtension.php
+++ src/Twig/Extension/ProductBundleOrderItemExtension.php
@@ -29,25 +29,48 @@ final class ProductBundleOrderItemExtension extends AbstractExtension
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('bitbag_render_product_bundle_order_items', [$this, 'renderProductBundleOrderItems'], ['is_safe' => ['html']]),
+            new TwigFunction('bitbag_render_admin_product_bundle_order_items', [$this, 'renderAdminProductBundleOrderItems'], ['is_safe' => ['html']]),
+            new TwigFunction('bitbag_render_shop_product_bundle_order_items', [$this, 'renderShopProductBundleOrderItems'], ['is_safe' => ['html']]),
         ];
     }
 
-    public function renderProductBundleOrderItems(OrderItemInterface $orderItem): string
+    public function renderAdminProductBundleOrderItems(OrderItemInterface $orderItem): string
+    {
+        $items = $this->getItems($orderItem);
+
+        if ($items === []) {
+            return '';
+        }
+
+        return $this->twig->render('@BitBagSyliusProductBundlePlugin/Admin/Order/Show/_productBundleOrderItems.html.twig', [
+            'items' => $items,
+        ]);
+    }
+
+    public function renderShopProductBundleOrderItems(OrderItemInterface $orderItem): string
+    {
+        $items = $this->getItems($orderItem);
+
+        if ($items === []) {
+            return '';
+        }
+
+        return $this->twig->render('@BitBagSyliusProductBundlePlugin/Shop/Order/Show/_productBundleOrderItems.html.twig', [
+            'items' => $items,
+        ]);
+    }
+
+    private function getItems(OrderItemInterface $orderItem): array
     {
         /** @var ProductInterface $product */
         $product = $orderItem->getProduct();
 
         if (!$product->isBundle()) {
-            return '';
+            return [];
         }
 
-        $items = $this->productBundleOrderItemRepository->findBy([
+        return $this->productBundleOrderItemRepository->findBy([
             'orderItem' => $orderItem,
-        ]);
-
-        return $this->twig->render('@BitBagSyliusProductBundlePlugin/Admin/Order/Show/_productBundleOrderItems.html.twig', [
-            'items' => $items,
         ]);
     }
 }
