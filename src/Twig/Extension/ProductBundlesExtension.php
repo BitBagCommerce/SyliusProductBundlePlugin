ccee940c45b5989aa374ef954108f1b3cb95fadd
Hubert Filar <hubert.filar@gmail.com>
2024-09-19 12:14:33 +0200

    OP-289: Add products in bundle section on admin product show page

--- src/Twig/Extension/ProductBundlesExtension.php
+++ src/Twig/Extension/ProductBundlesExtension.php
@@ -12,7 +12,9 @@ declare(strict_types=1);
 namespace BitBag\SyliusProductBundlePlugin\Twig\Extension;
 
 use BitBag\SyliusProductBundlePlugin\Entity\ProductBundleInterface;
+use BitBag\SyliusProductBundlePlugin\Entity\ProductBundleItemInterface;
 use BitBag\SyliusProductBundlePlugin\Entity\ProductInterface;
+use BitBag\SyliusProductBundlePlugin\Repository\ProductBundleItemRepositoryInterface;
 use BitBag\SyliusProductBundlePlugin\Repository\ProductBundleRepositoryInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
@@ -21,6 +23,7 @@ final class ProductBundlesExtension extends AbstractExtension
 {
     public function __construct(
         private readonly ProductBundleRepositoryInterface $productBundleRepository,
+        private readonly ProductBundleItemRepositoryInterface $productBundleItemRepository,
     ) {
     }
 
@@ -28,6 +31,7 @@ final class ProductBundlesExtension extends AbstractExtension
     {
         return [
             new TwigFunction('bitbag_get_bundles_containing_product', [$this, 'getBundlesForProduct'], ['is_safe' => ['html']]),
+            new TwigFunction('bitbag_get_products_from_bundle', [$this, 'getProductsFromBundle'], ['is_safe' => ['html']]),
         ];
     }
 
@@ -35,5 +39,11 @@ final class ProductBundlesExtension extends AbstractExtension
     public function getBundlesForProduct(ProductInterface $product): array
     {
         return $this->productBundleRepository->findBundlesByVariants($product->getVariants());
+    }
+
+    /** @return ProductBundleItemInterface[] */
+    public function getProductsFromBundle(ProductInterface $product): array
+    {
+        return $this->productBundleItemRepository->findByBundleCode((string) $product->getCode());
     }
 }
